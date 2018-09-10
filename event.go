f496a72f71e6d0a7e243fb2713d16b7c4c4c1a96
kristen <kristen.pire@epitech.eu>
2018-09-10 18:47:29 +0200

    add event routes

---
+++ event.go
@@ -0,0 +1,33 @@
+package coinbase
+
+import "time"
+
+type APIEventData struct {
+	Id           string     `json:"id,omitempty"`
+	Resource     string     `json:"ressource,omitempty"`
+	Created_at   *time.Time `json:"created_at,omitempty"`
+	Api_version	string `json:"api_version,omitempty"`
+	Data					APIChargeData `json:"data,omitempty"`
+
+}
+
+type APIEvent struct {
+	Data   APIEventData `json:"data,omitempty"`
+	Errors []Error       `json:"errors,omitempty"`
+}
+
+type APIEvents struct {
+	Pagination APIPagination   `json:"pagination,omitempty"`
+	Data       []APIEventData `json:"data,omitempty"`
+	Errors     []Error         `json:"errors,omitempty"`
+}
+
+func (a *APIClient) Event(id string) (event APIEvent, err error) {
+	err = a.Fetch("GET", "/events/"+id, nil, &event)
+	return
+}
+
+func (a *APIClient) Events() (events APIEvents, err error) {
+	err = a.Fetch("GET", "/events/", nil, &events)
+	return
+}
